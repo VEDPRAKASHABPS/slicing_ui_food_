857c79a8b38fe32ec2e50af4f79186b691070c1f
VED PRAKASH <125896446+VEDPRAKASHABPS@users.noreply.github.com>
2023-12-05 18:00:45 +0530

    Add files via upload

---
+++ lib/services/services_grocery.dart
@@ -0,0 +1,130 @@
+class GroceryServices {
+  static List products = [
+    {
+      "photo":"assets/ITEM/pizza.jpg",
+      "product_name": "Pizza",
+      "price": 200,
+      "category": "Food",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+
+          "assets/ITEM/burger.jpg",
+      "product_name": " Burger",
+      "price": 60,
+      "category": "Food",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/chowmein.jpg",
+      "product_name": "Chowmein",
+      "price": 60,
+      "category": "Food",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/fried-rice.jpg",
+      "product_name": "Fried Rice",
+      "price": 80,
+      "category": "Food",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/milk.jpg",
+      "product_name": " Milk",
+      "price": 32,
+      "category": "Drink",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/fried chicken.jpg",
+      "product_name": "Fried Chicken",
+      "price": 80,
+      "category": "Food",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/Drink.jpg",
+      "product_name": "Tea",    
+      "price": 62,
+      "category": "Drink",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/Orange Juice.jpg",
+      "product_name": "Orange Juice",
+      "price": 56,
+      "category": "Drink",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/Avocado Juice.jpg",
+      "product_name": "Avocado Juice",
+      "price": 56,
+      "category": "Drink",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/Puncak Coffe.jpg",
+      "product_name": "Puncak Coffe",
+      "price": 56,
+      "category": "Drink",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/Tea.jpg",
+      "product_name": "Silvana Tea",
+      "price": 40,
+      "category": "Drink",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      "photo":
+          "assets/ITEM/eggr.jpg",
+      "product_name": "Egg Roll",
+      "price": 50,
+      "category": "food",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+  ];
+
+  static List menuCategory = [
+    {"name": "Electronic", "image": "assets/icon/icon_electronic.png"},
+    {"name": "Fashion", "image": "assets/icon/icon_fashion.png"},
+    {"name": "F&B", "image": "assets/icon/icon_F&B.png"},
+    {"name": "Beauty", "image": "assets/icon/icon_Beauty.png"},
+    {"name": "Deals", "image": "assets/icon/icon_Deals.png"},
+    {"name": "Electronic", "image": "assets/icon/icon_electronic.png"},
+    {"name": "Fashion", "image": "assets/icon/icon_fashion.png"},
+    {"name": "F&B", "image": "assets/icon/icon_F&B.png"},
+  ];
+
+  static List banner = [
+    "assets/banner/banner_1.png",
+    "assets/banner/banner_2.png",
+    "assets/banner/3.png",
+    "assets/banner/4.png"
+  ];
+}
